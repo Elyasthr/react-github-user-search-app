7592d965a0f5570d9ab33a75288e659078a7a2fb
Elyas <touahria.elyas@outlook.com>
2022-02-06 12:45:44 +0100

    not found user is fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,13 @@ import HeaderCard from './HeaderCard';
 import BodyCard from './BodyCard';
 import FooterCard from './FooterCard';
 
-const Card = ({theme,userId}) => {
+const Card = (props) => {
 
+  const {theme,userId,onChange} = props
   const [userData,setUserData] = useState({});
-  
 
   useEffect(()=>{
     const neededValue = ['login','avatar_url','name','company','blog','location','bio','twitter_username','public_repos','followers','following','created_at'];
-
     axios
       .get('https://api.github.com/users/'+ userId)
       .then((res) => {
@@ -19,9 +18,13 @@ const Card = ({theme,userId}) => {
         const filteredArray = dataArray.filter(([key,value]) => neededValue.includes(key));
         const dataObject = Object.fromEntries(filteredArray);
         setUserData(dataObject);
+        onChange(0);
       })
-      .catch((err) => console.log(err))
-  },[userId])
+      .catch((err) => {
+        console.log(err);
+        onChange(err.response.status);
+      })
+  },[userId,onChange])
 
   return (
     <div className='card'>
