3b5412741f55baa3ee97543be6069ccd0570af9f
Elyas <touahria.elyas@outlook.com>
2022-02-07 10:43:34 +0100

    fix searchbar display

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,14 +10,15 @@ const SearchBar = (props) => {
   }
 
   return (
-    <div className='search-bar'>
-      <svg height="24" width="25" xmlns="http://www.w3.org/2000/svg"><path d="M10.609 0c5.85 0 10.608 4.746 10.608 10.58 0 2.609-.952 5-2.527 6.847l5.112 5.087a.87.87 0 01-1.227 1.233l-5.118-5.093a10.58 10.58 0 01-6.848 2.505C4.759 21.16 0 16.413 0 10.58 0 4.747 4.76 0 10.609 0zm0 1.74c-4.891 0-8.87 3.965-8.87 8.84 0 4.874 3.979 8.84 8.87 8.84a8.855 8.855 0 006.213-2.537l.04-.047a.881.881 0 01.058-.053 8.786 8.786 0 002.558-6.203c0-4.875-3.979-8.84-8.87-8.84z" fill="#0079ff"/></svg>
-      <form onSubmit={onSubmit}>
-        <input className="input-search" placeholder='Search Github username…' onChange={(e) => setUser(e.target.value)} />
+    
+      <form className='search-bar' onSubmit={onSubmit}>
+        <svg height="24" width="25" xmlns="http://www.w3.org/2000/svg"><path d="M10.609 0c5.85 0 10.608 4.746 10.608 10.58 0 2.609-.952 5-2.527 6.847l5.112 5.087a.87.87 0 01-1.227 1.233l-5.118-5.093a10.58 10.58 0 01-6.848 2.505C4.759 21.16 0 16.413 0 10.58 0 4.747 4.76 0 10.609 0zm0 1.74c-4.891 0-8.87 3.965-8.87 8.84 0 4.874 3.979 8.84 8.87 8.84a8.855 8.855 0 006.213-2.537l.04-.047a.881.881 0 01.058-.053 8.786 8.786 0 002.558-6.203c0-4.875-3.979-8.84-8.87-8.84z" fill="#0079ff"/></svg>
+        <input className={errorMessage ? "input-search input-search-bis"  : "input-search" }placeholder='Search Github username…' onChange={(e) => setUser(e.target.value)} />
+        {errorMessage !== 0 && <span className='not-found'>No results</span>}
         <button className='button-search' type="submit">Search</button>
       </form>
-      {errorMessage !== 0 && <span className='not-found'>No results</span>}
-    </div>
+      
+  
   );
 };
 
